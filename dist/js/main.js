112c2017ea92b8f5c5709c9eeec796648b7c151a
AdanCR <mglatinos@gmail.com>
2020-03-17 15:27:45 -0500

    menu mobile centrado

--- dist/js/main.js
+++ dist/js/main.js
@@ -39,20 +39,68 @@
 let toggle_menu = document.querySelector('.toggle-menu');
 let nav = document.querySelector('nav');
 
-toggle_menu.onclick = function() {
+
+
+toggle_menu.onclick = function () {
   nav.classList.toggle('show-menu');
-  
+
 }
 
 
 
 // back color header
-$(window).on("scroll", function() {
-  if($(window).scrollTop() > 50) {
-      $(".header2").addClass("active");
+$(window).on("scroll", function () {
+  if ($(window).scrollTop() > 50) {
+    $(".header2").addClass("active");
   } else {
-      //remove the background property so it comes transparent again (defined in your css)
-     $(".logo-navegation").removeClass("active");
+    //remove the background property so it comes transparent again (defined in your css)
+    $(".logo-navegation").removeClass("active");
   }
 });
 
+// ACORDION
+class Accordion {
+  constructor(element) {
+    this.element = document.querySelector(element);
+    this.groups = [];
+    this.element.querySelectorAll(".accordion__group").forEach(value => {
+      let group = new accordionGroup(value, this);
+      this.groups.push(group);
+    });
+  }
+
+  hideElse(e) {
+    this.groups.forEach(value => {
+      (value != e) ? value.hideGroup() : "";
+    })
+  }
+}
+
+class accordionGroup {
+  constructor(element, parent) {
+    this.element = element;
+    this.parent = parent;
+    this.status = false;
+    this.body = element.querySelector(".accordion__body");
+    this.header = element.querySelector(".accordion__header");
+    this.header.addEventListener("click", () => {
+      if (this.status) this.hideGroup();
+      else {
+        this.parent.hideElse(this);
+        this.showGroup();
+      }
+    });
+  }
+
+  showGroup() {
+    this.status = true;
+    this.body.classList.replace("is-inactive", "is-active");
+  }
+
+  hideGroup() {
+    this.status = false;
+    this.body.classList.replace("is-active", "is-inactive");
+  }
+}
+
+let a = new Accordion(".accordion");
